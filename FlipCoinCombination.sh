bf91f2f271079fe4856170f15b2626949bc951ea
navyas211@gmail.com <navyas211@gmail.com>
2021-09-04 22:11:06 +0530

    [NAVYA]ADDED: Flipping coin Triplet toss combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -15,18 +15,21 @@ fun(){
 value="$( fun $((RANDOM%2)) )"
 echo Output of singleToss time flip one coin $value
 
-declare -A doubletToss
+declare -A trippletToss
 
 for (( i=0; i<$T; i++ ))
 do
   value1="$( fun $((RANDOM%2)) )"
   value2="$( fun $((RANDOM%2)) )"
-  doubletToss[$value1$value2]=$((${doubletToss[$value1$value2]}+1))
+  value3="$( fun $((RANDOM%2)) )"
+  trippletToss[$value1$value2$value3]=$((${trippletToss[$value1$value2$value3]}+1))
 done
-echo Doublet combination ${!doubletToss[@]}
-echo Doublet count ${doubletToss[@]}
-for key in "${!doubletToss[@]}";
+
+echo Tripplet combination ${!trippletToss[@]}
+echo Tripplet count ${trippletToss[@]}
+
+for key in "${!trippletToss[@]}"
 do
-  prcntg=`expr "scale=2;${doubletToss[$key]}*100/$T" | bc`
+  prcntg=`expr "scale=2;${trippletToss[$key]}*100/$T" | bc`
   echo prcntg of $key $prcntg
 done
