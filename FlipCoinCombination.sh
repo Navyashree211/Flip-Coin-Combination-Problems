d7f74f0cda3abddd891082b2d73e2354eb122331
navyas211@gmail.com <navyas211@gmail.com>
2021-09-04 14:39:00 +0530

    [NAVYA]ADDED: Flip coin Siglet Combination In Dictionary

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,32 @@
 #!/bin/bash -x
 
-coinflip=$((RANDOM%2))
-IS_HEAD=1
-if [ $coinflip -eq 1 ]
-then
-   echo HEAD
-else
-   echo TAIL
-fi
+read -p "Enter the number of Trials : " T
+
+fun(){
+   number=$1
+   if (( $number == 0 ))
+   then
+       value=H
+   else
+       value=T
+   fi
+   echo $value
+}
+value="$( fun $((RANDOM%2)) )"
+echo Output of singleToss time flip one coin $value
+
+declare -A singletToss
+
+for (( i=0; i<$T; i++ ))
+do
+  value="$( fun $((RANDOM%2)) )"
+  singletToss[$value]=$((${singletToss[$value]}+1))
+done
+echo Singlet combination ${!singletToss[@]}
+echo Singlet count ${singletToss[@]}
+for key in "${!singletToss[@]}";
+do
+  prcntg=`expr "scale=2;${singletToss[$key]}*100/$T" | bc`
+  echo prcntg of $key $prcntg
+
+done
